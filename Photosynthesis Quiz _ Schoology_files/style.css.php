80d6788a1df68cfa26e1872605e04a9310fefab7
Rich77498 <65352426+Rich77498@users.noreply.github.com>
2021-09-27 17:24:17 -0500

    Add files via upload

---
+++ Photosynthesis Quiz _ Schoology_files/style.css.php
@@ -0,0 +1,162 @@
+/**
+	Variable: -name
+*/
+
+/* style */
+
+
+
+body .search-toggle { background-position: 4px -466px; } body #nav .dropdown { background-position: -3px -581px; } body #nav #primary-settings .dropdown { background-position: -4px -583px; }
+body #nav ul #home a { background: url(https://asset-cdn.schoology.com/sites/all/themes/schoology_theme/school_themes/system/files/imagecache/node_themes/sites/all/themes/schoology_theme/node_themes/675970269/BrandingFBiSD_57c046b23d3fa.png) no-repeat 0px 0px; }
+body #primary-messages span span { background-color: #902829; }
+body #header { background: #ECE8DF; }
+body #header-gradient {  }
+/* 
+body #nametag {  } 
+body #nametag a:hover { color: #902829;  }
+body #sub-nav { background: url(/header.) no-repeat;  }
+*/
+body .primary-activities .clickable, body #nav #primary-home a, body #nav #profile a, body #nav ul li a { color: #56565b; }
+body #nav ul li a:hover,
+body #nav #primary-home a:hover,
+body #nav #profile a:hover,
+body .primary-activities .clickable:hover,
+body .primary-activities .active,
+body #nav ul li.active-trail a,
+body .ac_results ul .ac_over .ac-top,
+body .schoology:hover,
+body .ac_over .schoology,
+body .action-links a:hover,
+body .action-links span:hover,
+body .edit-profile-picture-hover:hover,
+body .edit-profile-picture-hover.active,
+body #sidebar-left .action-links a:hover,
+.component-add-link:hover,
+body .search-toggle:hover,
+body #primary-settings .unfold:hover,
+#nav ul li a:hover {
+  color: #902829;
+  background-color: #F7F6F2;
+}
+
+.s-enable-course-dashboard.is-home #nav ul #primary-home,
+.s-enable-course-dashboard.s-home-course-dashboard #nav ul #primary-home,
+.s-enable-course-dashboard.is-home #nav ul #primary-home a,
+.s-enable-course-dashboard.s-home-course-dashboard #nav ul #primary-home a {
+  color: #902829;
+  background-color: #F7F6F2;
+}
+
+/* target only home, courses, group & resources left navigation buttons */
+#nav #nav_left li#primary-home:hover,
+#nav #nav_left li.primary-activities:hover,
+#nav #nav_left li.primary-activities.active {
+   background-color: #F7F6F2;
+}
+
+body #nav li#search input, body #nav ul#nav_left li#search #nav-search a { border-color:  }
+body .submit-span-wrapper, body .click-submit, body .submit-btn, body .popups-body .submit-span-wrapper, body #nav .s-notifications-mini .requester-links a:hover { background-image: none; border-color: #CCC8BF; background-color: #56565b; }
+body a, body .clickable, body .smart-box .filter-block li a, body #content-left-top .item-list li a, body .popups-tab, body .enrollment-filters span, body .grading-groups-list .grading-group, body #grading-group-create:hover { color: #902829; }
+body #nav .section-item-right a, body #nav .group-item-right a, body #nav .group-name { color: #902829; }
+body input[type="submit"].disabled { border-color:; }
+body .addthis_toolbox .vertical a { color: #902829; }
+body #footer { background: url(/sites/all/themes/schoology_theme/images/powered-by.png) no-repeat 50% 80%; }
+body .popups-buttons span:first-child { background-image: none; border-color: #CCC8BF; }
+body a.user:hover, body a.user:hover span.name-wrapper span { color: #902829; }
+body .multistep-breadcrumbs .current span { color: #902829; }
+body .ac_results { color: #902829; }
+body .select-btn-menu-title { background-image: none; }
+body .clickable.edit-profile-picture-hover { color: #fff; }
+#smart-box-more-wrapper .action-links-unfold span { color: #902829; }
+body .content-top-upper .course-title a.active { color: #902829; }
+body .more-btn { color: #902829; }
+body .enrollment-filters span.active, body .grading-groups-list .grading-group.active { background: #902829; }
+body #sidebar-left .access-code.left-block-wrapper .reset-code-wrapper a { color: #902829; }
+
+/* login */
+body.login-page .submit-span-wrapper { background-image: none; }
+body.login-page #header { background-color: transparent; }
+
+/* attendance */
+body .student-name a { color: #902829; }
+
+/* course manager */
+body #s-grade-editor tbody tr:hover { background-color: ; }
+body #s-grade-editor tbody tr:hover th { background-color: ; }
+body #s-grade-editor .student-name:hover b { color: #902829; }
+body #s-course-create-form .add-grading-period-btn { color: #902829; }
+
+/* smart box */
+body .smart-box .filter-block li .smartbox-boxtab, body .smart-box .filter-block li .personal-blog a { color: #902829; }
+
+/* Enrollment */
+body .people-filters li.active, .people-filters span.active { background-color: #CCC8BF; }
+
+/* Feeds */
+body .s-edge-feed .edge-footer .ajax-post-comment { color: #902829; }
+body .feed-comments-viewall-container { color: #902829; }
+
+/* Grades gradebook */
+body .grading-period-title { color: #902829; }
+
+/* Grade item */
+body #dropbox-submitted-filter-button { color: #902829; }
+
+/* Assessment */
+
+body .question-true_false .selected span, body .multiple-choice-review .selected span, body .ordering-review .selected span { color: #902829; }
+body .question-fitb .answer .selected { color: #902829; }
+body .question-matching .col-3, body .matching-review .col-3 { color: #902829; }
+body .student-name a:hover b { color: #902829; }
+body .score-grade-score span { color: #902829; }
+
+/* Group */
+body .s-group-invite-form .user-name-wrapper { color: #902829; }
+
+/* Home */
+body .welcome-message .welcome-box-list h3 { color: #902829; }
+body input#edit-hide { color: #902829; }
+
+/* Messaging */
+body tbody tr.odd.privatemsg-unread td, body tbody tr.even.privatemsg-unread td { background-color: ; }
+
+/* Page */
+body .create-btn a:hover { border-color: #CCC8BF; background-image: none; }
+
+/* School Admin */
+body .period-show-details { color: #902829; }
+
+/* Permissions */
+body #s-school-user-admin-perm-form thead th .perm-cat { color: #902829; }
+
+/* User manage */
+body .toggle-instructions { color: #902829; }
+body .edit-users-table input.form-text { color: #902829; }
+body #s-users-edit-filter-form #filter-form-title { color: #902829; }
+
+/* Calendar */
+body #right-column .has-events .month a { background-color: ; }
+
+/* District */
+body .district-sub-header { height: 65px; width: 500px; margin-bottom: 20px; background: ; }
+body .anonymous .district-sub-header { display: none; }
+body.anonymous #header { background: ; height: 59px; }
+
+/* Albums */
+body #edit-caption-btn, body span.tag-photo { color: #902829; }
+
+/* top right menus */
+body #nav .s-notifications-mini li a { color: #902829; }
+body #nav li ul.subnav a { color: #902829; }
+
+/* advisor */
+body #child-switcher .username-dropdown { color: #902829; }
+
+/* nav */
+body #nav ul li {  }
+body #nav li#search, body #nav #primary-notifications, body #nav #primary-requests, body #nav #private-messages, body #nav li#primary-settings {  }
+body #navigation_search, body .activities-dropdown-wrapper, body .request-wrapper, body .notification-wrapper, body .message-wrapper, body #nav li ul.subnav {  }
+body .search-toggle.active, body .search-toggle.active:hover, body .unread-messages.active, body .unread-reqs.active, body .unread-notifications.active {  }
+body #nav li#primary-settings .active {  }
+body .primary-activities .active {  }
+body .search-toggle, body #primary-settings .unfold { border-color: #CCC8BF;background-color: #ECE8DF; }
